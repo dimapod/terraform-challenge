f46ca5fa6c583047b090d1ce11e150ebd00c1ecb
dimapod <dimapod@gmail.com>
2017-01-09 16:12:14 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  access_key = "${var.access_key_challenge}"
+  secret_key = "${var.secret_key_challenge}"
+  region = "${var.region}"
+}
+
+resource "aws_key_pair" "monitoring" {
+  key_name = "${var.key_name}"
+  public_key = "${file("${var.key_name}.pub")}"
+}
+
+resource "aws_subnet" "monitoring" {
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "${var.private_subnet}"
+  map_public_ip_on_launch = "true"
+
+  tags {
+    Name = "${var.project} subnet"
+    Project = "${var.project}"
+  }
+}
+
+resource "aws_instance" "monitoring" {
+  key_name = "${var.key_name}"
+  ami = "${lookup(var.amis, var.region)}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.monitoring.id}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.monitoring.id}"
+  ]
+
+  tags {
+     Name = "${var.project}"
+     Project = "${var.project}"
+     Owner = "${var.owner}"
+  }
+}
+
+output "ip" {
+  value = "${aws_instance.monitoring.public_ip}"
+}
+
+
+
+
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "access_key_challenge" {}
+variable "secret_key_challenge" {}
+
+variable "project" {
+  type = "string"
+  default = "dpo-monitoring"
+}
+
+variable "key_name" {
+  type = "string"
+  default = "dpo-monitoring"
+}
+
+variable "region" {
+  default = "eu-central-1"
+}
+
+variable "vpc_id" {
+  type = "string"
+}
+
+variable "owner" {
+  type = "string"
+}
+
+variable "private_subnet" {
+  type = "string"
+}
+
+variable "amis" {
+  type = "map"
+  default = {
+    eu-central-1 = "ami-211ada4e"
+  }
+}
+
